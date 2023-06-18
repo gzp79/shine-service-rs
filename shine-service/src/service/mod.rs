fc549e780dea2a63e83f0118326d480272d9625e
gzp <->
2023-06-18 20:40:15 +0200

    embed to root CA certificates

--- shine-service/src/service/mod.rs
+++ shine-service/src/service/mod.rs
@@ -3,6 +3,8 @@ pub use self::session_key::*;
 mod user_session;
 pub use self::user_session::*;
 
+pub mod cacerts;
+
 pub const APP_NAME: &str = "Scytta";
 pub const DOMAIN_NAME: &str = "scytta.com";
 
